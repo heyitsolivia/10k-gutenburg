01c09888b1c9782178a983f08e894a1a62c5cbf2
Jan Rubio <rubiojan@gmail.com>
2016-09-24 23:29:38 -0400

    Updating title

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   var articles = [new Article(), new Article()];
     res.render('index', {
-      title: 'Generator-Express MVC',
+      title: '10K Gutenberg',
       articles: articles
     });
 });
